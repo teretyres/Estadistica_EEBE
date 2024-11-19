954b44a05a7d0f69d86e8114054a31a0bf91478b
teretyres <teresa.martin.alguacil@estudiantat.upc.edu>
2024-11-19 17:58:22 +0100

    Apuntes modelos discretos v_1.0 (antes de ejercicios)

---
+++ Q2/Modelos discretos.R
@@ -0,0 +1,54 @@
+## RESUMEN
+#d-> f(x)
+#p-> F(x)
+#r-> simular
+#q-> quantile
+
+#f(x)
+X<- 0:40
+graph<- dpois(X,lambda)
+plot(X, graph, type='h', col='red')
+#F(x)
+plot(X, ppois(X,lambda), type='s', col='red')
+#simular
+set.seed(123)#misma simu para la misma semilla
+sim<- rpois(n,lambda) #importante que la simu este anclada a una variable
+mean(sim) #media de simu
+median(sim)#mediana de simu
+var(sim)#variación de simu
+
+
+## BINOMIAL
+#pruebas repetidas con éxito/fracaso
+dbinom(x,n,p)
+E_b<-n*p
+Var_b<- n*p(1-p)
+
+
+## HIPERGEOMÉTRICA
+#Población finita de éxitos/fracasos, cuanso una unidad es seleccionada, esta disminuye en proporción
+
+#x= nº éxitos buscados
+#N= nº población total
+#k= nº éxitos totales
+#n= nº de repeticiones (sin reemplazo)
+dhyper(x,k,N-k,n)
+E_h<- n*k/N
+Var_h<- n*k/N*(1-k/N)*((N-n)/(N-1))
+
+
+## BINOMIAL NEGATIVA
+#nº de pruebas hasta un nº fijo de éxitos
+
+#x= nº de pruebas
+#r= nº éxitos
+dnbinom(x,r,p)
+E_nb<- r/p
+Var_nb<- r*(1-p)/p^2
+
+
+## POISSON
+#nº de resultados en un intervalo de tiempo
+E_p<- lambda
+Var_p<- lambda
+dpois(x, lambda)
