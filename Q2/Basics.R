d74f4f3cf5ed5a8e941849bcc24cae4ba2931f99
teretyres <teresa.martin.alguacil@estudiantat.upc.edu>
2024-11-19 18:08:25 +0100

    Básicos aprendidos del Q1

---
+++ Q2/Basics.R
@@ -0,0 +1,26 @@
+## BASICS
+X<- c(x1, x2, x3,...,x2) #vector
+rep(num, cuantas veces)
+sort(dades$seccio) #ordena - a +
+cut(dades$seccio, breaks=x) #cortar en x intervalos
+rm(list=ls()) #equivalente a clear all en matlab
+sum(X) #sumatorio
+seq(x1,x2,saltos, length.out=n) #secuencia de numeros; n= longitud de la secuencia (si usamos length.out no pueden haber saltos)
+head(x, n) #n primeros valores de x
+tail(x, n) #n ultmos valores de x
+
+## TABLA de FRECUENCIAS
+#Tabla de frecuencias
+ni<- table(dades$seccio) #cuantas veces sale cada cosa
+fi<- ni/sum(ni) #Frecuencias relativas
+Fi<- cumsum(fi) #suma acumulativa-> Frecuencia acumulada
+Ni<- cumsum(ni) #n acumulada
+cbind(ni, fi, Ni, Fi)
+
+## GRÁFICOS
+pie(fi)#Pie chart
+pie((tables(misdades)))
+boxplot(dades)
+dotchart(misdades)
+barplot((table(misdades)))
+scatterplot(yaxis xaxis, ((N-1)/N)*var(misdades))
