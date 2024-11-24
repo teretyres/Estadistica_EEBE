0ffcc0fe61716f67ddf974108171c2ea1fd037b9
teretyres <teresa.martin.alguacil@estudiantat.upc.edu>
2024-11-24 17:33:10 +0100

    Primer ejercicio T6 lab y corrección apuntes de muestreo

--- Q2/Muestreo.R
+++ Q2/Muestreo.R
@@ -19,3 +19,35 @@ dchisq((n-1)*s^2/sigmax^2, df=n-1) #n= tamaño de la muestra
 
 ## TLC
 #Si n>30, la distribución de la media y la suma muestral es normal.
+
+
+## FUNCIONES de DENSIDAD
+f <- function(x) {
+  ###función f(x)
+}
+curve(f,0,1,col='red', lwd=3, mai= 'función de densidad', xlab='x', ylab='f(x)')#graficar
+
+#SIMULAR (para n< 30, si no distr. normal)
+N<- 50 #nº de muestras
+n<- 4 #tamaño de muestras
+size1<- N*n
+
+#Matriz de valores
+x1<- seq(0,1,0.001)
+f1<- f(x1)
+set.seed(321)
+samples<- sample(x1, size1, replace=T, prob=f1)
+samples_x<-as.data.frame(matrix(samples, ncol=n))
+#función de distr. de suma
+sum_sample_x<- apply(samples_x,1,sum) #mean(sum_sample_x), var(sum_sample_x)
+
+#función de distr. media
+mean_sample_x<- apply(samples_x,1,mean) #mean(mean_sample_x),...
+
+#función de distr. varianza
+var_sample_x<- apply(samples_x,1,var) #mean(var_sample_x),...
+
+
+
+
+
--- Q2/T6_ateneaLAB.R
+++ Q2/T6_ateneaLAB.R
@@ -6,19 +6,44 @@ f <- function(x) {
   4 / (pi * (1 + x^2))
 }
 
-x_vals<- seq(0,1, length.out=500)
-probf<- f(x_vals)
+#graficar
+curve(f,0,1,col='red', lwd=3, mai= 'función de densidad', xlab='x', ylab='f(x)')
+
+#simulación
+N<- 50 #nº de muestras
+n<- 4 #tamaño de muestras
+size1<- N*n
+
+x1<- seq(0,1,0.001)
+f1<- f(x1)
 set.seed(321)
-sim<- sample(x_vals,4,replace=T, prob= probf)
+samples<- sample(x1, size1, replace=T, prob=f1)
+samples_x<-as.data.frame(matrix(samples, ncol=n))
 
-obs<- as.data.frame(matrix(sim, ncol=4))
-sum_obs<- apply(obs,1,sum)
-var(sum_obs)
+#función de distr. de suma
+sum_sample_x<- apply(samples_x,1,sum)
+mean(sum_sample_x)
+0.4412712*n #comprovación q sea la media sea n veces mayor
+var(sum_sample_x)
+0.07851927*n
+#como n menor a 30 no es distribución normal
 
-var_obs<- apply(obs,1,var)
-var_obs
-sqrt(var_obs)
-4*mu
+#función de distr. media
+mean_sample_x<- apply(samples_x,1,mean)
+mean(mean_sample_x)
+var(mean_sample_x)
+0.07851927/n
+
+#función de distr. varianza
+var_sample_x<- apply(samples_x,1,var)
+mean(var_sample_x)
+
+n2=50
+#se puede hacer distr. normal
+pnorm(24, n2*mu, sqrt(n2)*sigma)- pnorm(17, n2*mu, sqrt(n2)*sigma)
+pnorm(0.41, mu, sigma/sqrt(n2))- pnorm(0.39, mu, sigma/sqrt(n2))
+pchisq((n2-1)*0.15^2/sigma^2, df=n2-1)- pchisq((n2-1)*0.05^2/sigma^2, df=n2-1)
+
 
 #EJERCICIO 2
 
