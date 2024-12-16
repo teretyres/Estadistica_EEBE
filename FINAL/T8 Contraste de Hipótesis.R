5c98ed7002ee9c991c213421bcb64264d66c7499
teretyres <teresa.martin.alguacil@estudiantat.upc.edu>
2024-12-16 10:38:31 +0100

    Apuntes t8 v0.1

---
+++ FINAL/T8 Contraste de Hipótesis.R
@@ -0,0 +1,28 @@
+## CRITERIOS IC
+#1. IC
+# mu0 dentro del IC-> rechaza H1
+# mu0 fuera del IC-> rechaza H0
+
+#2. VALOR CRÍTICO
+# Zobs dentro de Zc-> rechaza H1
+Zc<- c(qnorm(alfa/2),qnorm(1-alfa/2))# 2 colas
+Zc<- qnorm(1-alfa)# cola sup.
+Zc<- qnorm(alfa)#cola inf.
+Zobs<- (xbar-mu0)/(sigmax/sqrt(n))
+
+#3. P-VALOR
+#p-valor dentro de 1-alfa-> rechaza H1
+pvalor<- 2*pnorm(-Zobs)# 2 colas
+pvalor<- pnorm(Zobs)# cola inf.
+pvalor<- 1-pnorm(Zobs)# cola sup.
+
+## ERRORES
+#Error I: P(Rechazar H0| H0 cierta)= alfa
+#Error: P(NO Rechazar H0| H0 falsa)= beta
+beta<- 2*pnorm(xbar, mu0, sqrt(sigma/n))# 2 colas
+beta<- 1-pnorm(xbar, mu0, sqrt(sigma/n))# cola sup.
+beta<- pnorm(xbar, mu0, sqrt(sigma/n)) # cola inf. 
+
+## ACIERTOS
+#No error: P(No rechazar H0| H0 cierta)= 1-alfa
+#Potencia: P(Rehazar H0| H0 falsa)= 1-beta
